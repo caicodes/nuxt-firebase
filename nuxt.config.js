9460ccb6880953b600f455bdb5fcad427c266a26
Cai Codes <caicodes1@gmail.com>
2021-05-15 22:26:50 -0600

    dev running checkpoint, no firebase, starting point after adding

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,25 +6,21 @@ export default {
     titleTemplate: '%s - nuxt-firebase',
     title: 'nuxt-firebase',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -39,6 +35,22 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+
+    '@nuxtjs/firebase',
+    {
+      config: {
+        apiKey: 'AIzaSyD7E2qZhaf7NvmaRhXAiaRaS5eZkFlK4Zs',
+        authDomain: 'caicodes-nuxt-firebase.firebaseapp.com',
+        projectId: 'caicodes-nuxt-firebase',
+        storageBucket: 'caicodes-nuxt-firebase.appspot.com',
+        messagingSenderId: '1029787023844',
+        appId: '1:1029787023844:web:b7d10db477c758cf036ff1',
+        measurementId: 'G-LL1916JW56',
+      },
+      services: {
+        auth: true, // Just as example. Can be any other service.
+      },
+    },
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -57,13 +69,12 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
